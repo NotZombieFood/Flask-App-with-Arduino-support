d92b5564eb275f6aaad39dc51d8e8cf4c1934d41
notZombieFood <gerardocd1000@gmail.com>
2019-04-28 15:34:26 -0500

    finally working

--- app/routes.py
+++ app/routes.py
@@ -2,36 +2,12 @@
 # cd Documents/LDI/Flask-App-with-Arduino-support
 from app import app
 from flask import render_template, redirect, url_for, request, send_from_directory, request
-import datetime, os, threading, serial, time
-datetime.datetime.now().strftime('%H:%M:%S')
+import os, pickle
 
-global lista_valores
-global lista_tiempos
-lista_valores = []
-lista_tiempos = []
-arduino = serial.Serial("COM3",9600,timeout = 2)
-# arduino.close()
-def checkVal():    
-    rawString = arduino.readline()
-    value = rawString.decode("utf-8").replace("\r","").replace("\n","")
-    return value
-
-def append(value):
-    lista_valores.append(value)
-    lista_tiempos.append(datetime.datetime.now().strftime('%H:%M:%S'))
-
-def f(f_stop):
-    valor = checkVal()
-    if valor != '':
-        if int(valor)>50:
-            append(int(valor))
-    if not f_stop.is_set():
-        # call f() again in 60 seconds
-        threading.Timer(.5, f, [f_stop]).start()
-
-f_stop = threading.Event()
-# start calling f now and every 60 sec thereafter
-f(f_stop)
+def getLista():
+    with open('app/listas.pickle', 'rb') as f:
+        file = pickle.load(f)
+    return file
 
 @app.route('/')
 def base():
@@ -43,20 +19,27 @@ def send_static_files(path):
 
 @app.route('/monitor')
 def loadMonitor():
-    return render_template('monitor.html', value = lista_valores[-1])
+    lista = getLista()
+    latest = lista["valores"][-1]
+    return render_template('monitor.html', value = latest)
 
 @app.route('/graph')
 def loadGraph():
-    return render_template('grapher.html', list = lista_valores, time_list = lista_tiempos)
+    lista = getLista()
+    print(str(lista["timestamps"]))
+    return render_template('grapher.html', list = lista["valores"], time_list = str(lista["timestamps"]))
 
 @app.route('/latest')
 def getLatestValue():
-    return str(lista_valores[-1])
+    lista = getLista()
+    latest = lista["valores"][-1]
+    return str(latest)
 
 
 @app.route('/values')
 def getValues():
-    return str(lista_valores)
+    lista = getLista()
+    return str(lista)
 
 
 @app.errorhandler(404)
